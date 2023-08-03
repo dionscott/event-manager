779834a529106b454bb4a5bd9decc3228956d5ef
Dion Scott <dionanthonyscott@gmail.com>
2023-08-03 10:03:37 -0500

    Find the peak registration hours

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5, '0')[0..4]
@@ -61,6 +62,19 @@ def normalize_phone_number(phone_number)
     end
 end
 
+def readable_date(date)
+    readable_date = date.split(" ")
+    only_date = readable_date[0].split("/")
+    new_year = only_date.last.prepend("20")
+    readable_date[0] = only_date.join("/")
+    readable_date.join(" ")
+end
+
+def peak_registration_hours(hours)
+    hours.max_by { |k,v| v }
+end
+
+
 puts 'EventManager initialized!'
 
 contents = CSV.open('event_attendees.csv', 
@@ -71,8 +85,10 @@ contents = CSV.open('event_attendees.csv',
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+# initialize a hash to find the highest hour registered
+hours_applied = Hash.new(0)
 contents.each do |row|
-    id = row[0]
+    # id = row[0]
     # name = row[:first_name]
 
     # zipcode = clean_zipcode(row[:zipcode])
@@ -82,16 +98,27 @@ contents.each do |row|
     # form_letter = erb_template.result(binding)
 
     # save_thank_you_letter(id, form_letter)
-    phone_number = row[:homephone]
-    
-    puts "#{id}: #{normalize_phone_number(phone_number)}"
-    # if phone < 10 bad
-    # if phone == 10 good
-    # if phone is 11 and 0 is 1, trim the 1 and its good
-    # if phone is 11 and 0 isn't 1 bad
-    # if phone > 11 bad
-    
-    
 
+    # phone_number = row[:homephone]
+    # puts "#{id}: #{normalize_phone_number(phone_number)}"
+
+    # find the regdate and time to show the peak hours that people reg
+    # find the hours
+    # keep tally in a hash of the times
+    # show times
+    date = row[:regdate]
+
+    p time = Time.strptime(readable_date(date), "%m/%d/%Y %k:%M")
+    p time.strftime("%Y") 
+
+    # add hour to hash to find common hour
+    hours_applied[time.strftime("%k")] += 1
+
+
+
+    # find the regdate that most people reg
+    
 
 end
+hours_applied
+puts peak_registration_hours(hours_applied)
